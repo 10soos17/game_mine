762858a6e70ebf5e992027998ec6611ae752db91
soos <shinsh1017@gmail.com>
2020-12-30 23:09:43 +0900

    segmentation faut error fix -> +++ def play_quit

--- mine_sound.py
+++ mine_sound.py
@@ -14,16 +14,18 @@ soundDir = os.path.join(baseDir, "mine_sound")
 effectMineDir = os.path.join(baseDir, "mine_effect/mine")
 effectVoiceDir = os.path.join(baseDir, "mine_effect/voice")
 
-CHUNK=1024
-AUDIO = pyaudio.PyAudio()
-
 def play_sound(song, replay):
-    global STREAM, PLAYNUM, STOP_FLAG
+    global STREAM, PLAYNUM, STOP_FLAG, QUIT_FLAG
     STOP_FLAG = False
+    QUIT_FLAG = False
     PLAYNUM = replay
+
     if PLAYNUM == "loop":
         PLAYNUM = 3
 
+    AUDIO = pyaudio.PyAudio()
+
+    CHUNK=1024
     WF = wave.open(song,'rb')
     HZ = WF.getframerate()
     CHANNELS = WF.getnchannels()
@@ -48,14 +50,21 @@ def play_sound(song, replay):
                 if STOP_FLAG == False:
                     STREAM.start_stream()
 
-            data = WF.readframes(CHUNK)
+            if QUIT_FLAG == True:
+                #print(f"QUIT_FLAG:{QUIT_FLAG}")
+                PLAYNUM = 0
+                break
 
+            data = WF.readframes(CHUNK)
 
         PLAYNUM -=1
 
         if PLAYNUM == 3:
             PLAYNUM +=1
 
+    STREAM.close()
+    AUDIO.terminate()
+    #print("sound quit")
     return 0
 
 def play_stop(flag):
@@ -64,7 +73,10 @@ def play_stop(flag):
 
 def play_restart():
     global STOP_FLAG
-    STOP_FLAG = False
+
+def play_quit():
+    global QUIT_FLAG
+    QUIT_FLAG = True
 
 def cal_duration(song):
     args=("ffprobe","-show_entries", "format=duration","-i",song)
@@ -80,24 +92,28 @@ def cal_duration(song):
 
 def play_msg(msg):
 
-    flag = False
-
 #    tts = gTTS(text=f"{msg}",lang="en",tld="com")
 #    tts.save(f"{effectDir}/tryagain.mp3")
 
     if msg == "Mission Failure":
+        play_quit()
+        #time.sleep(1)
         pls.playsound(f'{effectMineDir}/mine2.mp3', False)
         pls.playsound(f'{effectVoiceDir}/lose.mp3', False)
         duration = cal_duration(f"{effectVoiceDir}/lose.mp3")
         time.sleep(duration)
 
     elif msg == "Mission Complete":
+        play_quit()
+        #time.sleep(1)
         pls.playsound(f'{effectMineDir}/mine2.mp3', False)
         pls.playsound(f'{effectVoiceDir}/win.mp3', False)
         duration = cal_duration(f"{effectVoiceDir}/win.mp3")
         time.sleep(duration)
 
     elif msg == "Time Over":
+        play_quit()
+        #time.sleep(1)
         pls.playsound(f'{effectMineDir}/mine2.mp3', False)
         pls.playsound(f'{effectVoiceDir}/timeover.mp3', False)
         duration = cal_duration(f"{effectVoiceDir}/timeover.mp3")
@@ -113,7 +129,8 @@ def play_msg(msg):
         play_restart()
 
     elif msg == "Would you like to try again?":
-        #pls.playsound(f'{effectDir}/sinewave.mp3', False)
+        play_quit()
+        #time.sleep(1)
         pls.playsound(f'{effectVoiceDir}/question.mp3', False)
 
     elif msg == "YES":
